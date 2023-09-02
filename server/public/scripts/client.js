76849d62c971360464316cf61baece9d87f99c37
Abdirahman Jama <mustalid21@gmail.com>
2023-09-02 15:22:13 -0500

    post request on client and sever side are working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,33 +1,56 @@
-console.log('js works')
+// console.log("js works");
 
 $(document).ready(onReady);
 
-function onReady(){
-    getTasks();
-    console.log('jQuery works');
+function onReady() {
+  getTasks();
+//   console.log("jQuery works");
 
-    $('#addTask').on('click', addTask)
+  $("#addTask").on("click", addTask);
+}
 
-};
+function getTasks() {
+  $.ajax({
+    method: "GET",
+    url: "/todos",
+  })
+    .then((res) => {
+      console.log("GET tasks works:", res);
+      let tasks = res;
+      render(tasks);
+    })
+    .catch((err) => {
+      console.log("GET tasks err:", err);
+    });
+  //
+} //
 
-function getTasks(){
+function addTask(event) {
+  event.preventDefault();
+  let taskReceivedAsObject = {
+    title: $('#title').val(),
+    description: $('#description').val(),
+    is_complete: $('#isComplete').val()
+}
+    console.log('taksReceivedAsObject is:', taskReceivedAsObject);
 
     $.ajax({
-        method: 'GET',
-        url: '/todos'
-    }).then(
-        (res) => {
-            console.log('GET tasks works:', res);
-            let tasks = res;
-            render(tasks);
-        }).catch(
-            (err) => {
-                console.log('GET tasks err:', err);
-            })
-    // 
-}// 
-
-function render(tasks){
-    
-
+        type: 'POST',
+        url: '/todos',
+        data: {
+            title: "title", 
+            description: "description",
+            is_complete: "boolean"
+        }
+    }).then (function (response){
+        $('#title').val(''),
+        $('#description').val(''),
+        $('#isComplete').val('')
+        getTasks();
+    });
 }
+
+
+
+
+function render(tasks) {}
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -1,57 +1,64 @@
-const express = require('express');
+const express = require("express");
 const todosRouter = express.Router();
 
 // DB CONNECTION
-const pool = require('../public/scripts/modules/pool.js');
+const pool = require("../public/scripts/modules/pool.js");
 
-console.log('we are inside todos router');
+console.log("we are inside todos router");
 
 // GET
 
-todosRouter.get('/', (req, res) => {
-    console.log('against all odds, it worked!')
-    const sqlText = `
+todosRouter.get("/", (req, res) => {
+  console.log("against all odds, it worked!");
+  const sqlText = `
       SELECT * FROM "todos"
-    `
-    pool.query(sqlText)
+    `;
+  pool
+    .query(sqlText)
     .then((dbRes) => {
-        console.log('here is what we got back:')
-        console.log(dbRes.rows)
-        // dbRes.rows should evaluate to something like:
-        // [
-        //  {
-        //    id: 1,
-        //    text: 'take out the trash',
-        //    isComplete: false
-        //  },
-        //  {
-        //    id: 2,
-        //    text: 'eat supper',
-        //    isComplete: false
-        //  }
-        // ]
-})
-.catch((dbErr) => {
+      console.log("here is what we got back:");
+      console.log(dbRes.rows);
+      // dbRes.rows should evaluate to something like:
+      // [
+      //  {
+      //    id: 1,
+      //    text: 'take out the trash',
+      //    isComplete: false
+      //  },
+      //  {
+      //    id: 2,
+      //    text: 'eat supper',
+      //    isComplete: false
+      //  }
+      // ]
+    })
+    .catch((dbErr) => {});
+});
 
-})
-})
+// POST
+todosRouter.post("/", (req, res) => {
+  let newTask = req.body;
 
+  console.log("req.body:", req.body);
 
+  const queryText = `
+  INSERT INTO "todos" (title, description, is_complete)
+  VALUES ($1, $2, $3)
+  `;
 
-// POST 
-
-
-
-
+  pool
+    .query(queryText, [newTask.title, newTask.description, newTask.is_complete])
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(`Error making query ${queryText}`, err);
+      res.sendStatus(500);
+    });
+});
 
 // DELETE
 
-
-
-
-
-
 // PUT
-
 
 module.exports = todosRouter;
