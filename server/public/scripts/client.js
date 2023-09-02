1eb745a09e61f9c329281d8c37dae98036838506
Abdirahman Jama <mustalid21@gmail.com>
2023-09-02 18:16:47 -0500

    added put and delete request but not working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,6 +6,8 @@ function onReady() {
   //   console.log("jQuery works");
 
   $("#newTask").on("click", addTask);
+  $(".update-button").on("click", updateIsComplete);
+  $('#newTask').on('click', '.delete_button', deleteTask)
 }
 
 function getTasks() {
@@ -36,7 +38,7 @@ function addTask(event) {
   $.ajax({
     type: "POST",
     url: "/todos",
-    data: taskReceivedAsObject
+    data: taskReceivedAsObject,
   }).then(function (response) {
     getTasks();
     $("#title").val(""), $("#description").val(""), $("#isComplete").val("");
@@ -45,22 +47,99 @@ function addTask(event) {
 }
 
 function render(tasksFromServer) {
-  $('#addTaskTable').empty();
+  $("#addTaskTable").empty();
 
   for (let task of tasksFromServer) {
-    console.log('task we are looking at:', task);
+    console.log("task we are looking at:", task);
 
     let $newRow = $(`
     <tr>
       <td>${task.title}</td>
       <td>${task.description}</td>
-      <td>${task.is_complete}</td>
+      <td>
+        <select class="is-complete-dropdown">
+          <option value="false" ${
+            task.is_complete === "false" ? "selected" : ""
+          }>False</option>
+          <option value="true" ${
+            task.is_complete === "true" ? "selected" : ""
+          }>True</option>
+        </select>
+        <button class="update-button">Update</button>
+      </td>
+      <td><button class="delete_button">❌</button></td>
     </tr>
     `);
-    $newRow.data('id', task.id);
-    $('#addTaskTable').append($newRow);
-    console.log();
-    
-    
+    $newRow.data("id", task.id);
+    $("#addTaskTable").append($newRow);
   }
+}
+
+// function updateIsComplete() {
+//   let taskId = $(this).closest("tr").data("id");
+//   let newIsCompleteValue = $(this).siblings(".is-complete-dropdown").val();
+
+//   updateTask(taskId, newIsCompleteValue);
+// }
+
+function updateTask(taskIdToUpdate, newIsCompleteValue) {
+  let taskUpdateObject = {
+    is_complete: newIsCompleteValue,
+  };
+
+  console.log("taskUpdateObject:", taskUpdateObject);
+
+  $.ajax({
+    method: "PUT",
+    url: `/todos/${idToUpdate}`,
+    data: taskUpdateObject,
+  })
+    .then(function (response) {
+      console.log("Update task worked, task ID:", taskIdToUpdate);
+      getTasks();
+    })
+    .catch(function (error) {
+      alert("Error on updating task:", error);
+    });
+}
+
+// Modify the updateIsComplete function to call updateTask
+function updateIsComplete() {
+  let taskIdToUpdate = $(this).closest("tr").data("id");
+  let newIsCompleteValue = $(this).siblings(".is-complete-dropdown").val();
+
+  console.log("taskIdToUpdate:", taskIdToUpdate);
+  console.log("newIsCompleteValue:", newIsCompleteValue);
+
+  updateTask(taskIdToUpdate, newIsCompleteValue);
+}
+
+
+function deleteTask() {
+  // We don't want to remove it from the DOM.
+  // We want to destroy it's very essence.
+  // Whipe it from existance.
+  // We NEED the ID to make sure we are deleting the right thing.    
+
+  // We stored the id in a data attribute called 'id' on the table row
+  // 'this' is the delete button.
+  // We go up to the table row (this.parent.parent) and grab the id with the .data getter
+  let idToDelete = $(this).parent().parent().data('id');
+  console.log(idToDelete);
+
+  $.ajax({
+      method: 'DELETE',
+      url: `/todos/${idToDelete}` // We pass the id to the server in url as a url parameter
+  })
+      .then(
+          (results) => {
+              console.log('delete successful, this item no longer exists: ', idToDelete);
+              getTasks();
+          }
+      )
+      .catch(
+          (err) => {
+              alert("Error on delete, id:", idToDelete);
+          }
+      )
 }
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -4,12 +4,12 @@ const todosRouter = express.Router();
 // DB CONNECTION
 const pool = require("../public/scripts/modules/pool.js");
 
-console.log("we are inside todos router");
+// console.log("we are inside todos router");
 
 // GET
 
 todosRouter.get("/", (req, res) => {
-  console.log("against all odds, it worked!");
+  // console.log("against all odds, it worked!");
   const sqlText = `
       SELECT * FROM "todos"
     `;
@@ -18,7 +18,7 @@ todosRouter.get("/", (req, res) => {
     .then((dbRes) => {
       // console.log("here is what we got back:");
       // console.log(dbRes.rows);
-      res.send(dbRes.rows)
+      res.send(dbRes.rows);
     })
     .catch((dbErr) => {});
 });
@@ -47,6 +47,45 @@ todosRouter.post("/", (req, res) => {
 
 // DELETE
 
+todosRouter.delete("/:id", (req, res) => {
+  let idToDelete = req.params.id; // This is the id the client sends us in the url
+
+  let mySqlQuery = `
+    DELETE FROM tasks WHERE id = $1;
+    `;
+  pool
+    .query(mySqlQuery, [idToDelete])
+    .then((response) => {
+      console.log("delete request successful", idToDelete);
+      res.sendStatus(202);
+    })
+    .catch((err) => {
+      console.log(`delete request failed: ${idToDelete}`, err);
+      res.sendStatus(500);
+    });
+});
+
 // PUT
+
+todosRouter.put("/is_complete/:id", (req, res) => {
+  let taskId = req.params.id;
+  let updatedIsComplete = req.body.is_complete;
+
+  const queryText = `
+    UPDATE "todos"
+    SET "is_complete" = $1
+    WHERE "id" = $2
+  `;
+
+  pool
+    .query(queryText, [updatedIsComplete, taskId])
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log(`Error updating task with ID ${taskId}`, err);
+      res.sendStatus(500);
+    });
+});
 
 module.exports = todosRouter;
