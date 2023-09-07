705dc330c864ae84e346a69ad8a8294cef88c7bd
Abdirahman Jama <mustalid21@gmail.com>
2023-09-07 10:41:06 -0500

    added dotenv

--- server/public/scripts/modules/pool.js
+++ server/public/scripts/modules/pool.js
@@ -9,4 +9,29 @@ const pool = new pg.Pool({
     
 });
 
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+// {
+//     host: PGHOST,
+//     port: 5432,
+//     database: PGDATABASE,
+//     user: PGUSER,
+//     password: PGPASSWORD,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+
+
+
+// Spit out a console log when the pool connects
+// successfully:
+pool.on('connect', () => {
+    console.log('The magical pool thing connected to your postgres database. :)');
+})
+
+// Spit out a console log when the pool errors:
+pool.on('error', (error) => {
+    console.log('The magical pool has errored. Bummer.', error);
+})
+
 module.exports = pool;
